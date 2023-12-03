9971eb256e8052f3fc29b6591d16a74d36f2e778
Catron-Gainey <catrong23@gmail.com>
2023-12-03 02:56:54 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'password' 
+
+@app.route('/index')
+def index():
+    if 'count'not in session:
+        session['count'] = 0
+    session['count'] += 1
+    return render_template("index.html")
+
+@app.route('/destroy_session')
+def reset():
+    session.clear()
+    return redirect("/index")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
